b397d08c30d73942c5c006a68d8edc46ccf5d74a
Flavio Vidal <fbvidal@unb.br>
2022-06-05 09:32:58 -0300

    Including WoS data and results.

--- gendata-wos.py
+++ gendata-wos.py
@@ -4,25 +4,23 @@ import pycountry as pc
 import geopandas as gpd
 import folium
 
-# Loading data from CSV file to Pandas DataFrame
-scopus_data = pd.read_csv('data/Scopus/E6-scopus.csv')
-
-# Selecting colunms from DataFrame Affiliations and convert to string
-affiliation = str(scopus_data['Affiliations'].tolist())
+# Loading data from WoS standard exported txt files 
+with open('data/WoS/E6.txt', 'r') as f:
+    wos_data = f.read()
 
 # Read the geopandas countries dataset list
 world_countries_list = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
 # Extracting the name country from the affiliation data
 for c in pc.countries:
-    if c.name in affiliation:
+    if c.name in wos_data:
         # check if the country is in the list
         if c.name in world_countries_list['name'].tolist():
             # Get the index of the country in the list
             index = world_countries_list.index[world_countries_list['name'] == c.name].tolist()[0]
             # Add the number of papers in the country
-            world_countries_list.loc[index, 'papers'] = affiliation.count(c.name)
-        
+            world_countries_list.loc[index, 'papers'] = wos_data.count(c.name)
+
 # Cleaning the dataframe nan values
 world_countries_list = world_countries_list.dropna()
 
@@ -38,8 +36,8 @@ folium.Choropleth(
     fill_color='OrRd',
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name='Papers Published Scopus Expression E6'
+    legend_name='Papers Published Web of Science Expression E6'
 ).add_to(my_map)
 
 # Save the map
-my_map.save('out/Scopus/papers_scopus_E6.html')
+my_map.save('out/WoS/papers_scopus_E6.html')
